9c10e36e8ea3356727c8e0b9ec769a95ec81c584
rhettvj <rhettvj@gmail.com>
2018-12-06 19:24:49 -0500

    regular update

--- notams.py
+++ notams.py
@@ -3,7 +3,7 @@
 # export FLASK_DEBUG=1
 # flask run
 
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, Markup
 import requests
 import re
 import datetime
@@ -42,12 +42,13 @@ class CategoryParent:
 	name = ""
 	priority = 0
 	full_name = ""
+	hide = False
 
 #holds list of notams
 class Category:
 	name = ""
 	n_list = None
-	num_notams = 0	
+	num_notams = 0
 	priority = 0
 	full_name = ""
 	hide = False
@@ -63,6 +64,16 @@ class Category:
 		self.num_notams += 1
 	def get_priority():
 		return self.priority
+
+class Wx:
+	origin_code = ""
+	text = ""
+	category = None
+	alert = False
+	def __init__(self, origin_code, text):
+		self.origin_code = origin_code
+		self.text = text
+		self.category = "WX" #set for all wx lines
 
 class Notam:
 	origin_code = ""
@@ -80,21 +91,18 @@ class Notam:
 	issue_time_casual = ""
 	from_time_casual = ""
 	till_time_casual = ""
-	
+
 	recent = False
 	date_score = 0 #alters score based on date
-	
 	score = 0 #orders notams within a category
 	tags = "" #list of multiple tags
 	category = None #there can be only one
 	alert = False
-	
-	
 
 	def __init__(self, origin_code, origin_name, dt_issue_time, dt_from_time, dt_till_time, text):
 		self.origin_code = origin_code
 		self.origin_name = origin_name
-		
+
 		if dt_issue_time: self.dt_issue_time = dt_issue_time
 		if dt_from_time: self.dt_from_time = dt_from_time
 		if dt_till_time: self.dt_till_time = dt_till_time
@@ -130,8 +138,8 @@ def main():
 	notams = [] #used initially to store all notams and parse/tag/score
 	nfiles = None #dictionary of files into which they'll eventually be distributed
 					# filename : nFile
-	a_id = None	
-	category_parents_list = {}	
+	a_id = None
+	category_parents_list = {}
 	if request.method == 'POST':
 		result=request.form
 		a_id = result["aid"].upper()
@@ -141,25 +149,36 @@ def main():
 		raw_list = find_notams(html)
 		nfiles = {}
 		rule_list = load_rules("rules", category_parents_list)
-		
 		for raw_notam in raw_list:
-			n = parse_notam(raw_notam) 
+			n = parse_notam(raw_notam)
 			apply_rules(n, rule_list)
 			file_notam(n, nfiles, category_parents_list) #add notams to category lists within files
 		for f in nfiles.values(): #go thru each file
 			for c in f.c_list.values(): #go thru each category
-			 #sort each notam (notams are in [] lists)
+				#sort each notam (notams are in [] lists)
 				c.n_list = sorted(c.n_list, key=operator.attrgetter('score'))
 		#and then sort the categories themselves (categories are in {} dicts)
-		f.sorted_c_list = sorted(f.c_list.values(), key=operator.attrgetter('priority')) #key=operator.attrgetter('priority'))		
+			f.sorted_c_list = sorted(f.c_list.values(), key=operator.attrgetter('priority')) #key=operator.attrgetter('priority'))		
+
+
 	elif a_id and result["product"] == "weather":
 		html = download_file(a_id, "weather")
 		raw_list = find_weather_lines(html)
 		nfiles = {}
+		pcat = CategoryParent()
+		pcat.name = "WX"
+		pcat.full_name = "METAR-TAF"
+		pcat.priority = 0
+		category_parents_list["WX"] = pcat
 		for raw_wx in raw_list:
-			n = parse_wx(raw_wx, nfiles) #also builds the files list
-			#apply_casual_dates(n)
+			n = parse_wx(raw_wx)
 			file_notam(n, nfiles, category_parents_list)
+
+		for f in nfiles.values():
+			#this is a little hacky but there will be only be 1 category (WX)
+			#the template is expecting a 'view', not a dict and this seems like an easy way to 
+			#produce a view
+			f.sorted_c_list = sorted(f.c_list.values(), key=operator.attrgetter('priority'))
 
 	return render_template('header.html', files=nfiles, aid=a_id)
 
@@ -195,7 +214,7 @@ def apply_rules(n, rule_list):
 def file_notam(n, file_list, parent_list):
 	#create file for origin code if it doesn't already exist
 	if n.origin_code:
-		if n.origin_code not in file_list: 
+		if n.origin_code not in file_list:
 			file_list[n.origin_code] = nFile(n.origin_code)
 		#add to the notam to the file. the file itself will categorize it	
 		file_list[n.origin_code].add(n, parent_list)
@@ -208,7 +227,7 @@ def load_rules(filename, category_parents_list):
 	with app.open_resource(filename) as f:
 		rule_list = []
 		for line in f:
-			 if line:
+			if line:
 					line = line.decode('UTF-8') #because open_resource apparently spits out bytes
 					if line[0:1] == "#" or len(line) < 5:
 						continue
@@ -220,7 +239,7 @@ def load_rules(filename, category_parents_list):
 						this_parent_cat.hide = True if int(c_list[3]) > 0 else False
 						this_parent_cat.full_name = c_list[4]
 						category_parents_list[this_parent_cat.abbrev] = this_parent_cat
-					else:	
+					else:
 						this_rule = Rule()
 						r_list = line.split()
 						if len(r_list) < 4:
@@ -230,7 +249,7 @@ def load_rules(filename, category_parents_list):
 						this_rule.abbrev = r_list[1].replace("_", " ")
 						this_rule.category = r_list[2]
 						this_rule.score = int(r_list[3])
-						
+
 						if len(r_list) > 4:
 							this_rule.full = r_list[4].replace("_", " ")
 						rule_list.append(this_rule)
@@ -275,19 +294,64 @@ def find_notams(html):
 
 def find_weather_lines(html):
 	html = html.replace("\n", "*")
-	raw_list = re.findall('^SPECI.*\*|^METAR.*\*|^TAF.*=', html)
+	raw_list = re.findall('\*(?:TAF|SPECI|METAR).*?=', html)
 	return raw_list;
+
+def z_to_l(z_dt):
+	return z_dt.replace(tzinfo=timezone.utc).astimezone(tz=None)
+
 
 def parse_wx(raw_wx):
 	wxtype = ""
-	origin_code = ""
-	raw_issue_time = None
+	origin_code = None
+	raw_issue_time_l = None
+
 	text = ""
 	m_or_s = False
+	raw_wx = raw_wx[1:] #strip preceding *
+	raw_wx = raw_wx.replace("<br>", "")
 
 	#METAR/SPECI
-	if raw_wx[0:5] == 'METAR'
-  
+	if raw_wx[0:5] == 'METAR' or raw_wx[0:4] == 'SPECI':
+		wxtype = raw_wx
+		results = raw_wx.split()
+		origin_code = results[1]
+		x = 0
+		for word in results:
+			if len(word) == 7 and word[0:6].isdigit(): #051600Z
+				results[x] = wx_time_local(word[0:6])
+			x += 1
+		text = ' '.join(results)
+
+	elif raw_wx[0:3] == 'TAF':
+		wxtype = 'TAF'
+		results = raw_wx.split()
+		x = 0
+		for word in results:
+			flash ("." + word)
+			if origin_code is None and len(word) == 4:
+				origin_code = word
+			elif re.match("FM\d{6}", word) is not None: #FM052000
+				results[x] = "<br>" + "FM" + wx_time_local(word[2:8])
+			elif word == 'TEMPO':
+				results[x] = "<br>&nbsp;&nbsp;" + word
+			elif word == 'RMK':
+				results[x] = "<br>" + word
+			elif word == 'BECMG':
+				results[x] = "<br>" + word
+			elif len(word) == 7 and word[0:6].isdigit(): #051600Z
+				results[x] = wx_time_local(word[0:6])
+			elif re.match("\d{4}/\d{4}", word): #0210/0218
+				results[x] = wx_time_local(word[0:4]) + "/" + wx_time_local(word[5:9])
+			#elif raw_issue_time is None and len(word) == 6 and word.isdigit():
+			#	raw_issue_time = word
+			x += 1
+		text = Markup(' '.join(results))
+	text = text.replace("*", "")
+	text = text.lower()
+	return Wx(origin_code, text)
+
+
 def parse_notam(raw_notam):
 	raw_issue_time = None
 	raw_from_time = None
@@ -303,16 +367,16 @@ def parse_notam(raw_notam):
 	#if the above line found both a FROM and a TILL date
 	if date_match2 is not None:
 		raw_from_time = str(date_match2.group(1)) #take the FROM part
-	if date_match is not None: 
+	if date_match is not None:
 		raw_till_time = str(date_match.group(1))
-	
+
 	#Now do the rest
 	#there's no TIL date 
 	if raw_till_time is None:
-		parse_match = re.match("(\d{6}) (\D{4}) (.*?)\*(.*)",raw_notam)
+		parse_match = re.match("(\d{6}) (\w{4}) (.*?)\*(.*)",raw_notam)
 	else: #there is a TIL date, eat the date/time blocks at the end
-		parse_match = re.match("(\d{6}) (\D{4}) (.*?)\*(.*?)\d{10}",raw_notam)
-	
+		parse_match = re.match("(\d{6}) (\w{4}) (.*?)\*(.*?)\d{10}",raw_notam)
+
 	if parse_match[1]:
 		raw_issue_time = parse_match.group(1).replace("*", " ")
 	if parse_match[2]:
@@ -324,12 +388,52 @@ def parse_notam(raw_notam):
 			origin_code = origin_code + "-FIR"
 	if parse_match[4]:
 		text = " " + parse_match.group(4).replace("*", " ")
-		
+
 	n = Notam(origin_code, origin_name, parse_time(raw_issue_time), parse_time(raw_from_time), parse_time(raw_till_time), text)
 	return n;
 
+def wx_time_local(t):
+	today = "L"
+	tomorrow = "TMRW"
+	yesterday = "YSDY"
+	two_days_ago = "2DYSAGO"
+	TZ_DIFF = 5
+	now = datetime.datetime.now()
+	if t is None or int(t) == 0:
+		return None;
+	if t.isdigit():
+		if len(t) >= 4:
+			minute = 0
+			day = int(t[0:2])
+			hour = int(t[2:4])
+		if len(t) >= 6:
+				minute = int(t[4:6])
+	str_today = ""
+	#ex: 0200 Z tomorrow -5TZ
+	# (24+ (2-5)) = 2200Z yesterday
+	if hour-TZ_DIFF <= 0: #roll back the day by one (avoid caledar stuff by saying 'yesterday')
+		if day > now.day: #time given is showing as tomorrow, but is today local
+			flash("raw:" + t + "tdy")
+			str_today = today
+			hour = 24+(hour-TZ_DIFF)
+		elif day == now.day: #time given is showing today, but is actualy yesterday local
+			flash("raw:" + t + "yst")
+			str_today = yesterday
+			hour = 24+(hour-TZ_DIFF)
+		elif day < now.day: #time tdygiven shows yesterday, 
+			str_today = two_days_ago
+			hour = 24+(hour-TZ_DIFF)
+	else:
+		hour = hour-TZ_DIFF
+		if day == now.day+1:
+			flash("raw:" + t + "tmr")
+			str_today = tomorrow
+		elif day == now.day-1:
+			str_today = yesterday
+		elif day == now.day:
+			str_today = today
 
-
+	return '{:02d}{:02d}{}'.format(int(hour),int(minute),str_today)
 
 #Returns a datetime object from a given 6 or 10 digit string
 def parse_time(d):
@@ -342,7 +446,7 @@ def parse_time(d):
 			err("Attempted to parse a date with invalid month")
 			return None;
 		day = int(d[4:6])
-		if day > 31 or day < 1: 
+		if day > 31 or day < 1:
 			err("Attempted to parse a date with invalid day")
 			return None;
 		hour = 0
@@ -357,7 +461,7 @@ def parse_time(d):
 		return None
 
 def err(e):
-	global g_error_str 
+	global g_error_str
 	g_error_str = g_error_str + "\n" + e
 	return
 
@@ -367,7 +471,7 @@ def date_to_casual(d):
 	rstr = ""
 	today = datetime.datetime.utcnow()
 	delta = d - today
-	
+
 	#return only close days
 	if abs(delta.days) > 10:
 		return ""
@@ -375,7 +479,6 @@ def date_to_casual(d):
 	if delta.days == 1:
 		return "(" + str(abs(int(round(delta.seconds/(60*60))))) + " hours from now)"
 	elif delta.days == -1:
-		
 		return "(" + str(abs(int(round(delta.seconds/(60*60))))) + " hours ago)"
 	else:
 		rstr = "(" + str(abs(delta.days)) + " days"
